b6d870fe0e1d3d88eaca2f94b477d6767b614f3d
JoaquinGA01 <otrebla.2817@gmail.com>
2022-05-17 13:00:45 -0500

    .

--- Biblioteca/src/main/java/mx/uv/t4is/Biblioteca/Reservacion.java
+++ Biblioteca/src/main/java/mx/uv/t4is/Biblioteca/Reservacion.java
@@ -14,4 +14,62 @@ public class Reservacion {
         private String horaFin;
         
         private String tiempo;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getConcepto() {
+            return concepto;
+        }
+
+        public void setConcepto(String concepto) {
+            this.concepto = concepto;
+        }
+
+        public String getFecha() {
+            return fecha;
+        }
+
+        public void setFecha(String fecha) {
+            this.fecha = fecha;
+        }
+
+        public String getHoraInicio() {
+            return horaInicio;
+        }
+
+        public void setHoraInicio(String horaInicio) {
+            this.horaInicio = horaInicio;
+        }
+
+        public String getHoraFin() {
+            return horaFin;
+        }
+
+        public void setHoraFin(String horaFin) {
+            this.horaFin = horaFin;
+        }
+
+        public String getTiempo() {
+            return tiempo;
+        }
+
+        public void setTiempo(String tiempo) {
+            this.tiempo = tiempo;
+        }
+
+        
 }
