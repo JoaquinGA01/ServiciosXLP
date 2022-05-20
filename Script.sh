71e4d9351fab90270474d79d203f5dfc7c8cc549
JoaquinGA01 <otrebla.2817@gmail.com>
2022-05-20 12:36:50 -0500

    despliegue en heroku

--- Script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/etc/init.d/mysql start
-java -jar /app/ToDoList-0.0.1-SNAPSHOT.jar
---
+++ Subir/Script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/etc/init.d/mysql start
+/usr/bin/java -jar -Dserver.port=$PORT Biblioteca-0.0.1-SNAPSHOT.jar
