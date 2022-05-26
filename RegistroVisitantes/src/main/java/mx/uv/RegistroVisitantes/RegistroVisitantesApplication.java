a7c008bd83e620d75480b49a7818abd8b91a6207
JoaquinGA01 <otrebla.2817@gmail.com>
2022-05-26 16:28:37 -0500

    .

---
+++ RegistroVisitantes/src/main/java/mx/uv/RegistroVisitantes/RegistroVisitantesApplication.java
@@ -0,0 +1,13 @@
+package mx.uv.RegistroVisitantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroVisitantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroVisitantesApplication.class, args);
+	}
+
+}
---
+++ RegistroVisitantes/src/test/java/mx/uv/RegistroVisitantes/RegistroVisitantesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.RegistroVisitantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroVisitantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
