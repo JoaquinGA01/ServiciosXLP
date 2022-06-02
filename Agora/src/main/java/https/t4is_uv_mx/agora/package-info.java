f3e42bbebf491b5ddedcd1ee7cf565b889cc2d1a
AridaiOrtegaZ <aridaiortega1402@gmail.com>
2022-06-02 08:45:06 -0500

    agora pagina

--- Agora/src/main/java/https/t4is_uv_mx/agora/package-info.java
+++ Agora/src/main/java/https/t4is_uv_mx/agora/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.05.24 a las 09:54:32 PM CDT 
+// Generado el: 2022.06.02 a las 01:39:41 AM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://t4is.uv.mx/agora", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
